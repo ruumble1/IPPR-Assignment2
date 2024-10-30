978832c38f7c2c7ebde755b0606f8d193d19cd87
Gene <e.you8655@gmail.com>
2024-10-30 22:40:02 +1100

    updates

--- unet_load_data.py
+++ unet_load_data.py
@@ -2,10 +2,10 @@ import os
 import tensorflow as tf
 
 # Define directories
-positive_images_dir = r'Data/positive/images'
-positive_masks_dir = r'Data/positive/masks'
-negative_images_dir = r'Data/negative/images'
-negative_masks_dir = r'Data/negative/masks'
+positive_images_dir = r'archive\Positive\Images'
+positive_masks_dir = r'archive\Positive\Masks'
+negative_images_dir = r'archive/Concrete/Negative/Images'
+negative_masks_dir = r'archive/Concrete/Negative/Mask'
 
 # Function to load images and masks
 def load_negative(images_dir, masks_dir):
@@ -54,10 +54,10 @@ def load_positive(images_dir, masks_dir):
 
 
 # Load positive data
-positive_images, positive_masks = load_data(positive_images_dir, positive_masks_dir)
+positive_images, positive_masks = load_positive(positive_images_dir, positive_masks_dir)
 
 # Load negative data
-negative_images, negative_masks = load_data(negative_images_dir, negative_masks_dir)
+negative_images, negative_masks = load_negative(negative_images_dir, negative_masks_dir)
 
 # Combine positive and negative data
 images = tf.concat([positive_images, negative_images], axis=0)
